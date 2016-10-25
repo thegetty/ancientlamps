2b5e69150f74600c0e93ae973a3950402bfb6b31
Eric Gardner <gardner.ec@gmail.com>
2016-10-25 12:25:09 -0700

    Enable extension-less mode
    Moving the basic helper methods into the config file for now, and
    removing the next/prev helper links to enable the site to run in simpler
    mode. Will gradually re-introduce the extension plugins.

--- config.rb
+++ config.rb
@@ -39,6 +39,58 @@ activate :deploy do |deploy|
 end
 
 helpers do
+  def author_name
+    author = data.book.creators.first
+    %(#{data.book.creators[0].first_name} #{data.book.creators[0].last_name} and
+    #{data.book.creators[1].first_name} #{data.book.creators[1].last_name})
+  end
+
+  def citation_author
+    page = current_page.data
+    return default_author unless page.author
+    if page.author.size > 1
+      %(#{page.author[0].last_name}, #{page.author[0].first_name},
+          and #{page.author[1].first_name} #{page.author[1].last_name})
+    else
+      "#{page.author.first.last_name}, #{page.author.first.first_name}"
+    end
+  end
+
+  def default_author
+    book = data.book
+    if book.creators.size > 1
+      %(#{book.creators[0].last_name}, #{book.creators[0].first_name},
+          and #{book.creators[1].first_name} #{book.creators[1].last_name})
+    else
+      "#{book.creators.first.last_name}, #{book.creators.first.first_name}"
+    end
+  end
+
+  def book_info_chicago
+    book = data.book
+    path = current_path.gsub("index.html", "")
+    %(
+        In <em>#{book.title.main}</em>,
+        by #{author_name}.
+        #{book.publisher_location}:
+        #{book.publisher},
+        #{book.pub_date.year}.
+        <span class="force-wrap">#{permalink}/#{path}</span>.
+      )
+  end
+
+  def book_info_mla
+    book = data.book
+    path = current_path.gsub("index.html", "")
+    %(
+        <em>#{book.title.main}</em>. By #{author_name}.
+        #{book.publisher_location}:
+        #{book.publisher_short}, #{book.pub_date.year}.
+        <span class="cite-current-date">DD Mon. YYYY</span>
+        <<span class="force-wrap">#{permalink}/#{path}</span>>.
+      )
+  end
+
   def baseurl
     if environment? :development
       ""
@@ -49,5 +101,27 @@ helpers do
 
   def og_image_path
     "http://www.getty.edu#{baseurl}/assets/images/og_cover.jpg"
+  end
+
+  def page_title
+    title   = data.book.title
+    authors = data.book.creators
+    page    = current_page.data
+
+    if page.cat
+      if page.cat.is_a? Array
+        "Cats. #{page.cat.first}-#{page.cat.last} | #{title.short}"
+      else
+        "Cat. #{page.cat} | #{title.short}"
+      end
+    elsif page.sort_order.zero?
+      "#{title.main} | #{authors.first.first_name} #{authors.first.last_name}"
+    else
+      "#{page.title} | #{title.short}"
+    end
+  end
+
+  def permalink
+    data.book.editions.find { |edition| edition.name == "Online" }.link
   end
 end
