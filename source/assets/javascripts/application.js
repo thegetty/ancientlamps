7ff272b2892a5827b1410546828a97b622a66fba
Eric Gardner <gardner.ec@gmail.com>
2016-11-08 11:14:46 -0800

    Update JS to use modular style
    Application.js and ui.js have been refactored to utilize the modern
    module-based style that will be the new normal for this application.
    Some global plugins still need to be properly shimmed (leaflet-deepzoom,
    velocity.js, smoothState, etc). Added basic documentation to functions
    as well.

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,16 +1,23 @@
-//= require_tree .
-// L.Icon.Default.imagePath = '<%= baseurl %>/assets/stylesheets/vendor/leaflet/images';
+// =============================================================================
+// application.js
+// -----------------------------------------------------------------------------
+// This is the main module which serves as the entry point for all JS code in
+// the application. Webpack will point to this file and use it to compile a
+// dependency graph for all other JS assets during build.
+//
+// jQuery is presumed to be available globally, but all other dependencies for
+// this file should be explicitly required here.
 
+// Dependencies
+// -----------------------------------------------------------------------------
 var ui = require('./ui.js')
 
-// Make this available globally
-// var RegionMap;
-
-$(document).ready(function() {
-  // set up UI
-  ui()
-
-  // smoothState init
+// PrepareTransitions
+// -----------------------------------------------------------------------------
+// This function choreographs the SmoothState and Velocity animations between
+// page transitions. Make sure to re-initialize things like event-handlers after
+// new content has been loaded.
+function prepareTransitions() {
   $('#main').smoothState({
     onStart: {
       duration: 400,
@@ -29,14 +36,12 @@ $(document).ready(function() {
       ui()
     }
   })
+}
+
+// Document Ready
+// -----------------------------------------------------------------------------
+// Only call other functions inside of this.
+$(document).ready(function() {
+  ui()
+  prepareTransitions()
 })
-
-// function ready(fn) {
-//   if (document.readyState !== 'loading') {
-//     fn()
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn)
-//   }
-// }
-
-// ready(ui)
