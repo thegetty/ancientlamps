b7ab2615cdbc741eb5aff27ed2e9744393a54fa7
Eric Gardner <gardner.ec@gmail.com>
2016-11-29 16:49:44 -0800

    Remove gulp and replace with Webpack
    This is way simpler and seems to create fewer issues; Webpack handles JS
    resources, middleman handles everything else.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ source "https://rubygems.org"
 
 gem "middleman", "~> 4.0"
 gem "middleman-deploy", "= 2.0.0.pre.alpha"
+gem "middleman-livereload"
 
 gem "sanitize"
 gem "slugify"
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var Clean = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    application: './source/assets/javascripts/application.js'
+  },
+
+  resolve: {
+    root: __dirname + '/source/assets/javascripts',
+  },
+
+  output: {
+    path: __dirname + '/.tmp/dist',
+    filename: 'assets/javascripts/[name].js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /source\/assets\/javascripts\/.*\.js$/,
+        exclude: /node_modules|\.tmp|vendor/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0'],
+        },
+      },
+    ],
+  },
+
+  node: {
+    console: true,
+  },
+
+  plugins: [
+    new Clean(['.tmp']),
+  ],
+};
