8fa5c130995d9425348c363d4158cc7bf03bafb0
Eric Gardner <gardner.ec@gmail.com>
2017-01-04 16:27:24 -0800

    Update reference linker to work on essays

--- bin/reference_linker.rb
+++ bin/reference_linker.rb
@@ -7,6 +7,7 @@ references = YAML.load_file('./data/references.yml')
 catalogue = YAML.load_file('./data/catalogue.yml')
 keys = ['bibliography', 'parallels']
 
+# Wrap the Catalogue.yml bib references in links
 catalogue.each do |e|
   references.each do |r|
     ref_name = r['name']
@@ -26,7 +27,27 @@ catalogue.each do |e|
   end
 end
 
-# first_five = catalogue[0..4]
-# puts first_five.to_yaml
+# Write the file (uncomment when using)
+# File.open('./data/catalogue.yml', 'w') { |f| f.puts catalogue.to_yaml }
 
-File.open('./data/catalogue.yml', 'w') { |f| f.puts catalogue.to_yaml }
+# Wrap the Essay references in links
+essays = Dir.glob(["source/**/*.html.md", "source/**/*.html.md.erb"])
+essays.each do |essay|
+  essay_text = File.read(essay)
+  references.each do |r|
+    ref_name = r['name']
+    ref_id   = r['id']
+    regex    = /#{ref_name}(?!I)/
+
+    # Check the text of each essay for a match to the :name field of the references
+    m = essay_text.match(regex)
+    next if m.nil?
+
+    # If a match is found, wrap it in a link to the :id of the matched author.
+    essay_text.gsub!(m[0], "<a href='../../bibliography/##{ref_id}'>#{m[0]}</a>")
+  end
+
+  # Write the file (uncomment when using)
+  # File.open(essay, 'w') { |f| f.puts essay_text }
+  puts essay_text
+end
