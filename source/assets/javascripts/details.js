e0cfeb8d82413136a1ed926303cf21dda773ca22
Eric Gardner <gardner.ec@gmail.com>
2017-05-03 11:03:52 -0700

    Basic Details component
    The Details component uses Vue's "watch" feature to automatically update its
    template whenever the "cat" value changes. This data can be passed in
    programatically / thru console, or it can be triggered by an event outside
    happening outside of the component.

---
+++ source/assets/javascripts/details.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import _ from 'lodash/core'
+
+let Details = Vue.extend({
+  name: 'Details',
+  template: '#cat-entry-template',
+  data: function() {
+    return {
+      cat: '',
+      // dataURL: 'https://gettypubs.github.io/ancient-lamps/catalogue.json'
+      dataURL: '/catalogue.json',
+      entry: 'Loading'
+    }
+  },
+  watch: {
+    // Watch the cat number for changes
+    cat: function(newCat) {
+      this.entry = 'Loading'
+      this.getData()
+    }
+  },
+  computed: {
+    collectionLink: function() {
+      return `http://www.getty.edu/art/collection/objects/${this.entry.dor_id}`
+    },
+    stamp: function() {
+      if (this.entry.stamp) {
+        return `../../assets/images/stamps/${this.entry.stamp}`
+      } else {
+        return null
+      }
+    }
+  },
+  mounted: function() {
+    this.getData()
+    console.log('Mounted!')
+  },
+  methods: {
+    getData: function() {
+      $.get(this.dataURL).done((data) => {
+        this.entry = _.find(data, { 'cat_no': this.cat })
+      })
+    }
+  }
+})
+
+export default Details
