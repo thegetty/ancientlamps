58d020e86059e83bca39f2d41519104cd92dcb2d
Eric Gardner <gardner.ec@gmail.com>
2017-05-03 15:57:14 -0700

    Search component with localstorage now working
    This is now a standard Vue component, using the Watch method for populating its
    results. The contents list (which the Lunr results are cross-referenced against
    for a human-readible result) is stashed in LocalStorage when the component first
    loads. Thereafter it checks localStorage first, and only loads the JSON again if
    necessary.

--- source/assets/javascripts/search.js
+++ source/assets/javascripts/search.js
@@ -1,36 +1,63 @@
+import Vue from 'vue'
+import _ from 'lodash/core'
 import lunr from 'lunr'
 
-class Search {
-  constructor() {
-    this.index = this.buildIndex()
-    this.dataURL = 'https://gettypubs.github.io/ancient-lamps/search.json'
-    // this.dataURL = '/search.json'
+let Search = Vue.extend({
+  name: 'Search',
+  template: '#search-results-template',
+  data () {
+    return {
+      dataURL: '/search.json',
+      // dataURL: 'https://gettypubs.github.io/ancient-lamps/search.json'
+      index: this.buildIndex(),
+      results: [],
+      query: ''
+    }
+  },
+  created () {
+    console.log('Search created!')
+  },
+  mounted () {
     this.getData()
-    this.contentList = []
-  }
-
-  buildIndex() {
-    return lunr(function() {
-      this.field('cat', { boost: 1000 })
-      this.field('url')
-      this.field('title', { boost: 10 })
-      this.field('content')
-      this.ref('id')
-    })
-  }
-
-  getData() {
-    $.get(this.dataURL, {cache: true}).done((data) => {
-      this.contentList = data
-      data.forEach((item) => {
-        this.index.add(item)
+  },
+  watch: {
+    query (newQuery) {
+      this.results = []
+      this.results = this.search(newQuery)
+    }
+  },
+  methods: {
+    buildIndex () {
+      return lunr(function () {
+        this.field('cat', { boost: 1000 })
+        this.field('url')
+        this.field('title', { boost: 10 })
+        this.field('content')
+        this.ref('id')
       })
-    })
+    },
+    addItemToIndex (item) {
+      this.index.add(item)
+    },
+    getData () {
+      let storedContents = window.localStorage.getItem('contents')
+      if (storedContents) {
+        JSON.parse(storedContents).forEach((item) => { this.addItemToIndex(item) })
+      } else {
+        $.get(this.dataURL).done((data) => {
+          data.forEach((item) => { this.addItemToIndex(item) })
+          window.localStorage.setItem('contents', JSON.stringify(data))
+        })
+      }
+    },
+    search (query) {
+      this.query = query
+      let contents = JSON.parse(window.localStorage.getItem('contents'))
+      return _.map(this.index.search(query), (r) => {
+        return contents[r.ref]
+      })
+    }
   }
-
-  search(query) {
-    return this.index.search(query)
-  }
-}
+})
 
 export default Search
