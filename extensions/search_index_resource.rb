1c9fbeabfd73633cea7557f263787f90ec00dfad
Eric Gardner <gardner.ec@gmail.com>
2017-05-08 12:14:44 -0700

    Add Catalogue entries to search index
    The search feature now supports searching inside of catalogue entries in
    addition to content pages. This has caused the search index to more than double
    in size, so hopefully it is worth it.
    
    The search.json builds an index that contains an accurate URL for each catalogue
    entry, followed by an anchor link to its cat number. The next step is to cause
    this URL value to trigger the modal catalogue detail state.

--- extensions/search_index_resource.rb
+++ extensions/search_index_resource.rb
@@ -5,6 +5,7 @@ module Middleman
     class SearchIndexResource < ::Middleman::Sitemap::Resource
       def initialize(store, path)
         super(store, path)
+        @index = []
       end
 
       def template?
@@ -16,12 +17,12 @@ module Middleman
       end
 
       def render(_opts = {}, _locs = {})
-        page_index = build_page_index
-        page_index.to_json
+        build_page_index
+        build_catalogue_index
+        @index.to_json
       end
 
       def build_page_index
-        index = []
         pages = @app.sitemap.resources.find_all { |p| p.data.sort_order }
         pages.each_with_index do |resource, id|
           next if resource.data['index'] == false
@@ -33,13 +34,42 @@ module Middleman
             content: Sanitize.fragment(resource.render(layout: false))
           }
 
-          index.push(item)
+          @index.push(item)
         end
-        index
       end
 
       def lookup_entry(cat)
         @app.data.catalogue.find { |entry| entry.cat == cat }
+      end
+
+      def lookup_page_path_for_entry(cat)
+        page = @app.data.lookup_table.find do |p|
+          Array(p.cat_no).flatten.include? cat
+        end
+
+        "/catalogue/#{page.path}"
+      end
+
+      def build_catalogue_index
+        starting_id = @index.size
+        @app.data.catalogue.each_with_index do |entry, id|
+          next if entry.cat_no == "TBD"
+          item = {
+            id: starting_id + id,
+            title: "Cat. #{entry.cat_no}",
+            url: @app.config.baseurl + lookup_page_path_for_entry(entry.cat_no) + "##{entry.cat_no}",
+            content: [
+              entry.date,
+              entry.description,
+              entry.discus_iconography,
+              entry.parallels,
+              entry.place,
+              entry.discussion
+            ].join(" ")
+          }
+
+          @index.push(item)
+        end
       end
     end
   end
