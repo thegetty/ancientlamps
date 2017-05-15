954bf0a81bb034cc94d3e65c48a3a2a5ec37b4a3
Eric Gardner <gardner.ec@gmail.com>
2017-05-15 16:24:09 -0700

    Basic date filter working
    This is operating on raw data currently, will start styling the component next.

--- source/assets/javascripts/catalogue.js
+++ source/assets/javascripts/catalogue.js
@@ -9,16 +9,24 @@ let Catalogue = Vue.extend({
   },
   template: `
     <div>
-      <h1>Entries: {{entries.length}}</h1>
-      <vue-slider
-        ref="slider"
-        :min="slider.min"
-        :max="slider.max"
-        :interval="slider.interval"
-        :tooltip="slider.tooltip"
-        tooltip-dir="bottom"
-        v-model="date">
-      </vue-slider>
+      <div v-if="ready">
+        <h1>Entries: {{results.length}}</h1>
+        <vue-slider
+          ref="slider"
+          :min="slider.min"
+          :max="slider.max"
+          :interval="slider.interval"
+          :tooltip="slider.tooltip"
+          tooltip-dir="bottom"
+          v-model="date">
+        </vue-slider>
+        <ul>
+          <li v-for="item in results">{{ item }}</li>
+        </ul>
+      </div>
+      <div v-else>
+        Loading...
+      </div>
     </div>
   `,
   data () {
@@ -26,12 +34,18 @@ let Catalogue = Vue.extend({
       dataURL: '/catalogue.json',
       date: [-1000, 1000],
       entries: [],
+      ready: false,
       slider: {
         min: -1000,
         max: 1000,
         interval: 50,
         tooltip: 'hover'
       }
+    }
+  },
+  computed: {
+    results () {
+      return this.filterByDate()
     }
   },
   mounted () {
@@ -42,12 +56,27 @@ let Catalogue = Vue.extend({
       let storedCatalogue = window.localStorage.getItem('catalogue')
       if (storedCatalogue) {
         this.entries = JSON.parse(storedCatalogue)
+        this.results = this.entries
+        this.ready = true
       } else {
         $.get(this.dataURL).done((data) => {
           window.localStorage.setItem('catalogue', JSON.stringify(data))
           this.entries = data
+          this.results = this.entries
+          this.ready = true
         })
       }
+    },
+    filterByDate () {
+      return _
+        .chain(this.entries)
+        .filter('date_numeric')
+        .filter((entry) => {
+          return entry.date_numeric[0] >= this.date[0]
+        })
+        .filter((entry) => {
+          return entry.date_numeric[1] <= this.date[1]
+        }).value()
     }
   }
 })
