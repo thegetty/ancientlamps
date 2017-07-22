212512be9fdb1c496b1e969c8a48fd1a4f44a7e8
Eric Gardner <gardner.ec@gmail.com>
2017-07-21 17:15:25 -0700

    Set URL for review.getty.edu deployment

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -41,7 +41,7 @@ window.page = {
     search: '/search.json',
     catalogue: '/catalogue.json',
     plates: '/plates.json',
-    prefix: 'https://gettypubs.github.io/ancient-lamps'
+    prefix: 'https://review.getty.edu/publications/ancientlamps'
   },
 
   // Methods
